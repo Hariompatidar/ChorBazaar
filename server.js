e0fa6bf2e650b173b80cccf418f2576020a673d8
Hariompatidar <83593413+Hariompatidar@users.noreply.github.com>
2023-08-29 11:50:21 +0530

    Bug fixing

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 //configure env
